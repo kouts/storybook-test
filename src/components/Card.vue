357aaa4dcf34c556b47fd8caa48a256c35ed3fc7
kouts <koutsaftakis@gmail.com>
2020-10-09 23:42:27 +0300

    Added card title and content props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,7 +5,7 @@
     </div>    
     <img src="lion.jpg" class="card-img-top" alt="Lion" />
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">{{ title }}</h5>
       <p class="card-text">
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
@@ -21,6 +21,14 @@ export default {
     headerTitle: {
       type: String,
       default: ''
+    },
+    title: {
+      type: String,
+      default: 'Card title'
+    },
+    content: {
+      type: String,
+      default: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     }
   }
 };
